c75f06c18e4f6ad91eea86cb5d07c62990735980
santicano <canosantiago404@gmail.com>
2023-06-04 16:38:00 -0300

    Añadidos mas tests a capitalize

--- code/capitalize.js
+++ code/capitalize.js
@@ -1,4 +1,6 @@
 export default function capitalize(str) {
+    if (!str) return "Not a string";
+    
     let string = str.charAt(0).toUpperCase() + str.slice(1);
     return string;
 }
--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -3,3 +3,19 @@ import capitalize from "../code/capitalize.js";
 test('capitalizes a string', () => {
     expect(capitalize('hola')).toBe('Hola');
 })
+
+test('capitalizes a string', () => {
+    expect(capitalize('hola yo soy santi')).toBe('Hola yo soy santi');
+})
+
+test('no change in uppercase string', () => {
+    expect(capitalize('HOLA')).toBe('HOLA');
+})
+
+test('Not a string', () => {
+    expect(capitalize()).toBe("Not a string");
+})
+
+test('Not a string', () => {
+    expect(capitalize(null)).toBe("Not a string");
+})
